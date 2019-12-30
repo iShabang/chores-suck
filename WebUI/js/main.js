55ed6ab265b47e0fede1f505e58530b2f4b57d59
ishabang <montos2@unlv.nevada.edu>
2019-12-29 20:14:29 -0800

    removing unused files

--- WebUI/js/main.js
+++
@@ -1,5 +0,0 @@
-function loadTable(){
-    var xhttp = XMLHttpRequest()
-    xhttp.open('GET','/chores?',true)
-
-}
--- headers/ItemTime.h
+++
@@ -1,9 +0,0 @@
-#ifndef ITEM_TIME_H
-#define ITEM_TIME_H
-
-class ItemTime
-{
-    
-}
-
-#endif // ITEM_TIME_H
