874136c4638f95f6883f5bbc00ad6e59f0dfea6d
Shabang <montos2@unlve.nevada.edu>
2020-04-04 21:44:15 -0700

    adding design docs

--- webserver/tools/GroupHandler.go
+++ webserver/tools/GroupHandler.go
@@ -1,3 +1,19 @@
+/*
+A Group is a group of users that share chores. The chores are added to the
+group and then distributed among the users. When a chore is assigned to a
+user, it must be associated with a group (via group-id).
+
+Each group has an administrator (just defined by a user id) that controls who is in the group and the chores in it.
+Each group will have an array of users (not including the administrator). The administrator will also be assigned chores.
+
+Administrator functions:
+Create a group - this is techinically not an "administrator" privelage since anyone can create a group, but it must be included in this handler. The user that creates the group is the default administrator.
+Change the group name
+Add/remove users from the group
+Add/remove chores from the group
+
+*/
+
 package tools
 
 import (
@@ -7,8 +23,13 @@ import (
 	"net/http"
 )
 
-type GroupName struct {
-	String string `json:"name"`
+GroupChore struct {
+	Name
+}
+
+type GroupJson struct {
+	Name string `json:"name"`
+	Chores 
 }
 
 type GroupHandler struct {
@@ -29,13 +50,12 @@ func (h *GroupHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		h.CreateGroup(w, r)
 	case "/group/update":
 		h.UpdateGroup(w, r)
-	case "/group/find":
-		h.FindGroup(w, r)
-	case "/group/join":
-		h.JoinGroup(w, r)
 	}
 }
 
+/*
+The CreateGroup method will be restricted to authorized users only. Any user that creates a group will be the default administrator.
+*/
 func (h *GroupHandler) CreateGroup(w http.ResponseWriter, r *http.Request) {
 	id, success := h.auth.AuthorizeRequest(r)
 	if !success {
@@ -43,11 +63,11 @@ func (h *GroupHandler) CreateGroup(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var name GroupName
-	json.NewDecoder(r.Body).Decode(&name)
+	var gj GroupJson
+	json.NewDecoder(r.Body).Decode(&gj)
 
 	group := db.Group{
-		Name:  name.String,
+		Name:  gj.Name,
 		Admin: id,
 	}
 	_, err := h.conn.AddGroup(&group)
@@ -60,33 +80,22 @@ func (h *GroupHandler) CreateGroup(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
+/* Administrators will update fields in a group using a form. We will need to
+* get all the data in JSON format and commit the changes to the database. */
 func (h *GroupHandler) UpdateGroup(w http.ResponseWriter, r *http.Request) {
+	// First make sure this is a valid user
 	id, success := h.auth.AuthorizeRequest(r)
 	if !success {
 		w.WriteHeader(http.StatusUnauthorized)
 		return
 	}
 
+	// TODO: Grab all fields needed to update
 	var name GroupName
 	json.NewDecoder(r.Body).Decode(&name)
 
+	// Before commiting these changes, we need to ensure that this user is an
+	// administrator for this group. Find the group in the database and compare
+	// the administrator id with the current user id
+
 }
-
-func (h *GroupHandler) FindGroup(w http.ResponseWriter, r *http.Request) {
-	id, success := h.auth.AuthorizeRequest(r)
-	if !success {
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-
-	var name GroupName
-	json.NewDecoder(r.Body).Decode(&name)
-
-	group, err := h.conn.FindUserGroup(id, name.String)
-	if err != nil {
-		fmt.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-	}
-}
-
-func (h *GroupHandler) JoinGroup(w http.ResponseWriter, r *http.Request) {}
