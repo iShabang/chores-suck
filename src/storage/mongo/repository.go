5099c77c13ea729f53164cc0cee3d473bd3a8257
iShabang <curtin.shannon@gmail.com>
2021-01-11 02:31:51 -0800

    add session repo implementation

--- src/storage/mongo/repository.go
+++ src/storage/mongo/repository.go
@@ -3,10 +3,13 @@ package mongo
 import (
 	"chores-suck/types"
 	"context"
+	"errors"
 	"time"
 
+	"github.com/gorilla/sessions"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // Storage defines properties of a storage object
@@ -25,6 +28,45 @@ func (s *Storage) GetUserByName(name string) (types.User, error) {
 	return u, err
 }
 
+// GetSession fetches a session frm the database by session id
+func (s *Storage) GetSession(ses *sessions.Session) error {
+	filter := bson.D{{Key: "sid", Value: ses.ID}}
+	r, e := s.findOne(&filter, "sessions")
+	if e == nil {
+		e = r.Decode(ses)
+	}
+	return e
+}
+
+// DeleteSession deletes a session from the database by ID
+func (s *Storage) DeleteSession(ses *sessions.Session) error {
+	filter := bson.D{{Key: "sid", Value: ses.ID}}
+	e := s.deleteOne(&filter, "sessions")
+	return e
+}
+
+// UpsertSession updates an existing session or inserts a new one.
+func (s *Storage) UpsertSession(ses *sessions.Session) error {
+	query := bson.M{"sid": ses.ID}
+	update, err := bson.Marshal(ses)
+	var options options.UpdateOptions
+	options.SetUpsert(true)
+	s.upsert(&query, update, &options, "sessions")
+	return err
+}
+
+func (s *Storage) upsert(query *bson.M, update []byte, options *options.UpdateOptions, coll string) error {
+	collection := s.cl.Database("chores-suck").Collection(coll)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	res, err := collection.UpdateOne(ctx, query, update, options)
+	if err == nil && res.ModifiedCount < 1 {
+		err = errors.New("no objects modified")
+	}
+	return err
+
+}
+
 func (s *Storage) findOne(filter *bson.D, coll string) (*mongo.SingleResult, error) {
 	collection := s.cl.Database("chores-suck").Collection(coll)
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
@@ -35,4 +77,16 @@ func (s *Storage) findOne(filter *bson.D, coll string) (*mongo.SingleResult, err
 	}
 
 	return res, nil
+}
+
+func (s *Storage) deleteOne(f *bson.D, coll string) error {
+	var err error
+	collection := s.cl.Database("chores-suck").Collection(coll)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	res, err := collection.DeleteOne(ctx, f)
+	if err == nil && res.DeletedCount < 1 {
+		err = errors.New("No items deleted")
+	}
+	return err
 }
