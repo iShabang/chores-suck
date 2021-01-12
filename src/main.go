f6ffbf13465eb19a549f363ce492dcaebe57231f
iShabang <curtin.shannon@gmail.com>
2021-01-11 20:21:49 -0800

    Wrap serve mux to prevent memory leak from gorilla sessions

--- src/main.go
+++ src/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/context"
 )
 
 func main() {
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", context.ClearHandler(http.DefaultServeMux)))
 }
