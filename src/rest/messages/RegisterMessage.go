de4495baf34fdac722db46a6358db2729418063b
iShabang <curtin.shannon@gmail.com>
2021-02-11 18:39:23 -0800

    adding messages for form validation

---
+++ src/rest/messages/RegisterMessage.go
@@ -0,0 +1,8 @@
+package messages
+
+// RegisterMessage contains messages correlating to different parts of the register form
+type RegisterMessage struct {
+	Username string
+	Email    string
+	Password string
+}
