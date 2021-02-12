138f870b4f5d91d1b1780475125e7f6916fae7f6
iShabang <curtin.shannon@gmail.com>
2021-02-11 18:39:58 -0800

    adding validation function for form data

---
+++ src/rest/auth/validate.go
@@ -0,0 +1,46 @@
+package auth
+
+import (
+	"chores-suck/rest/messages"
+	"regexp"
+	"strings"
+)
+
+func validateInput(username string, p1 string, p2 string, email string, msg *messages.RegisterMessage) bool {
+	valid := true
+
+	regEmail := regexp.MustCompile(`.+@.+\..+`)
+	regName := regexp.MustCompile(`^(?:[0-9a-zA-Z]+-)+[0-9a-zA-Z]+$`)
+
+	if strings.TrimSpace(username) == "" {
+		msg.Username = "Must enter a valid username"
+		valid = false
+	}
+
+	if p1 != p2 {
+		msg.Password = "Passwords don't match"
+		valid = false
+	}
+
+	if strings.TrimSpace(p1) == "" {
+		msg.Password = "Must enter a password"
+		valid = false
+	}
+
+	if strings.TrimSpace(email) == "" {
+		msg.Email = "Must enter an email address"
+		valid = false
+	}
+
+	if !regName.MatchString(username) {
+		msg.Username = "Username must only consist of alphanumeric characters and hyphens and cannot start or end with a hyphen"
+		valid = false
+	}
+
+	if !regEmail.MatchString(email) {
+		msg.Email = "Must enter a valid email address"
+		valid = false
+	}
+
+	return valid
+}
