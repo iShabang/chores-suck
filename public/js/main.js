494353d6f25ab0f864c80248733fbfb8c7cb9ec5
iShabang <curtin.shannon@gmail.com>
2021-03-31 14:12:24 -0700

    updated sidebar js function, added style and separation to update group page

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,19 @@
-function sidebarClick(p1,p2,p3,p4) {
-    var rs = getComputedStyle(document.querySelector(':root'))
-    document.getElementById(p1).style.backgroundColor = rs.getPropertyValue('--clr-primary-200');
-    document.getElementById(p2).style.backgroundColor = rs.getPropertyValue('--clr-primary-600');
-    document.getElementById(p3).style.display = 'none';
-    document.getElementById(p4).style.display = 'block';
-}
-
-function sidebarChores() {
-
-}
-
-function sidebarGroups() {
-
+function sideClick(sel,show) {
+    var rs = getComputedStyle(document.querySelector(':root'));
+    var c1 = rs.getPropertyValue('--clr-primary-600');
+    var c2 = rs.getPropertyValue('--clr-primary-200');
+    //find each header. Change to default style
+    headers = document.getElementsByClassName('s-head');
+    for (h of headers) {
+        h.style.backgroundColor = c1;
+    }
+    //find the selected header. Change to selected style.
+    document.getElementById(sel).style.backgroundColor = c2;
+    //find each content div and hide it
+    divs = document.getElementsByClassName('v-content');
+    for (d of divs) {
+        d.style.display = 'none';
+    }
+    //show the selected content
+    document.getElementById(show).style.display = 'block';
 }
