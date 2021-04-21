200c327b6a47af5b6c220fec5c40c65674e8d084
ishabang <montos2@unlv.nevada.edu>
2021-04-21 10:33:58 -0700

    removed show class. use session storage for ui state

--- public/js/main.js
+++ public/js/main.js
@@ -3,29 +3,12 @@ function setupShow() {
     for (e of elems) {
         e.style.display = 'none';
     }
-    showElem = document.getElementsByClassName('show');
-    showElem[0].style.display = 'block';
-    showElem[0].classList.toggle('show');
-}
-
-function sideClick(sel,show) {
-    var rs = getComputedStyle(document.querySelector(':root'));
-    var c1 = rs.getPropertyValue('--clr-primary-600');
-    var c2 = rs.getPropertyValue('--clr-primary-200');
-    //find each header. Change to default style
-    headers = document.getElementsByClassName('s-head');
-    for (h of headers) {
-        h.style.backgroundColor = c1;
+    showElem = document.getElementById('first');
+    tab = window.sessionStorage.getItem('dashTab');
+    if (tab) {
+        showElem = document.getElementById(tab);
     }
-    //find the selected header. Change to selected style.
-    document.getElementById(sel).style.backgroundColor = c2;
-    //find each content div and hide it
-    divs = document.getElementsByClassName('v-content');
-    for (d of divs) {
-        d.style.display = 'none';
-    }
-    //show the selected content
-    document.getElementById(show).style.display = 'block';
+    showElem.style.display = 'block';
 }
 
 function display(showElem) {
@@ -34,6 +17,7 @@ function display(showElem) {
         d.style.display = 'none';
     }
     document.getElementById(showElem).style.display = 'block';
+    window.sessionStorage.setItem('dashTab',showElem);
 }
 
 setupShow()
